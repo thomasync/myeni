8139ecdc7ccb7f3f85582e9b698b1c22f24f6bb8
Thomas Cauquil <thomascauquil@MacBook-Pro-de-Thomas.local>
2020-12-27 17:18:26 +0100

    fix / add list of books / readme in french

--- README.md
+++ README.md
@@ -1,36 +1,40 @@
 # MyENI
 
-Download your books stored on your eni account, in EPUB or PDF.
+Téléchargez les livres de votre compte ENI en EPUB/PDF
 
-## Usage...
-  
-### Get the info
+## Utilisation...
 
- 1. Log in to your ENI account.
- 2. Go to the first page of your book (the url should look like this : http://www.eni-training.com/client_net/mediabook.aspx?idR=xxxxxxx)
- 3. Right click on the page then inspect and go console. Or <kbd>Cmd</kbd> + <kbd>Option</kbd> + <kbd>J</kbd> / <kbd>Ctrl</kbd> + <kbd>Shift</kbd> + <kbd>J</kbd>
- 4. Write `document.cookie`
+### Récupérer les infos
 
-### Use
+1. Connectez-vous à votre compte ENI
 
-1. `git clone https://github.com/absmoca/myeni` in terminal
-2. `cd myeni && npm install`
-3. `node myeni.js`
-4. Then enter the information you previously retrieved with `document.cookie`
-5. When you will be asked for the id of the book it will be the number in the url after *?IdR=**xxxxx***
+2. Clique-droit sur la page puis inspecter, aller dans l'onglet Console. Ou <kbd>Cmd</kbd> + <kbd>Option</kbd> + <kbd>J</kbd> / <kbd>Ctrl</kbd> + <kbd>Shift</kbd> + <kbd>J</kbd>
+
+4. Tapez `document.cookie`
+
+### Utiliser
+
+1.  `git clone https://github.com/absmoca/myeni` dans le terminal
+
+2.  `cd myeni && npm install`
+
+3.  `node myeni.js`
+
+4. Coller le contenu de `document.cookie` récupéré précédemment.
+
+5. Choisir le livre à télécharger puis son format.
+
+## À propos de la loi
 
 
-## About the law...
+La loi garantit aux ayants droit d'une œuvre (créateurs, artistes-interprètes et producteurs) un contrôle exclusif sur son exploitation.
 
-In France, the copyright (called "Droit d'auteur") law has an exception called "private copy" ("Copie privée") :
+Cela implique que toute reproduction ou représentation d'une œuvre ou d'une prestation doit être soumise à l'accord préalable de ses ayants droit.  
+Toutefois, la loi prévoit quelques exceptions où leur autorisation préalable n'est pas nécessaire, dont notamment l'exception pour "copie privée".
 
-You may create a copy of something if the goal is a private usage. Two origins are public diffusion and bought things.
+L'expression couramment employée de "copie privée" vise, au sens de la loi ([art. L 122-5 2° du CPI](https://www.copiefrance.fr/files/documents_de_reference/extraits_cpi_et_autres_dispositions_applicables_remuneration_copieprivee_janvier2012.pdf "CPI")) « les copies ou reproductions strictement réservées à l’usage privé du copiste et non destinées à une utilisation collective » (voir également l'[art. L 211-3 2° du CPI](https://www.copiefrance.fr/files/documents_de_reference/extraits_cpi_et_autres_dispositions_applicables_remuneration_copieprivee_janvier2012.pdf "CPI")  en ce qui concerne les droits voisins). La jurisprudence définit la notion d’usage privé comme un usage non commercial et personnel, ce qui exclut l'utilisation collective de la copie (en entreprise par exemple).  
+Cela signifie qu'un particulier est autorisé à copier une œuvre déjà fixée sur un support sur un autre support, pourvu que la destination et l'utilisation de cette copie lui soient strictement personnelles.
 
-So, you are not able to use this PoC to publish a book on a hidden network :)
-
-*taken here: https://github.com/Nainterceptor/ENIDownloader*
-
-## More information...
-- The application is in French, but not very hard to understand there are only a few words
-- If you download several books, you will probably have to retrieve the cookies each time because the site uses timestamps.
-- Last time i used it and it worked: *21/12/2020*
+Ce principe a été réaffirmé lors de la transposition en droit français ([loi nº 2006-961 du 1er août 2006](https://www.copiefrance.fr/files/documents_de_reference/loi_1_aout_2006.pdf "Loi du 1er août 2006")) de la directive européenne de 2001 relative au droit d'auteur à l'ère numérique (1). A cette occasion, des précisions ont été apportées quant à l'application de l'exception pour copie privée, notamment :  
+- comme toutes les exceptions prévues par l'article L 122-5 du CPI, elle est subordonnée au respect de deux conditions issues du concept international du "triple test" (2), à savoir ne pas "porter atteinte à l'exploitation normale de l'œuvre", "ni causer un préjudice injustifié aux intérêts légitimes de l'auteur" ([art. L 122-5 9° al.4 et L 331-9 du CPI](https://www.copiefrance.fr/files/documents_de_reference/extraits_cpi_et_autres_dispositions_applicables_remuneration_copieprivee_janvier2012.pdf "CPI")).  
+- par ailleurs, les titulaires de droit ont la possibilité de mettre en place des  [mesures techniques](https://www.copiefrance.fr/fr/ressources/questions-reponses "Questions-réponses")  permettant la limitation du nombre de copies ([art. L 331-9 du CPI](https://www.copiefrance.fr/files/documents_de_reference/extraits_cpi_et_autres_dispositions_applicables_remuneration_copieprivee_janvier2012.pdf "CPI")), sans qu'aucun nombre minimal de copies ne soit précisé. Ils doivent prendre "cependant les dispositions utiles pour que leur mise en œuvre ne prive pas les bénéficiaires des exceptions visées à l'article L. 331-8 du CPI de leur exercice effectif", dont l'exception pour copie privée.
--- myeni.js
+++ myeni.js
@@ -7,211 +7,291 @@ const epub = require('epub-gen');
 const request = require('request');
 const readline = require("readline");
 
-
-const cookies = [];
-
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-rl.question("Cookies: ", (arg_cookies) => {
-    arg_cookies = arg_cookies.replace(/"/g, "").replace(/\s/g, "").split(";")
-    arg_cookies.forEach((arg_cookie) => {
-        arg_cookie = arg_cookie.match(/(.*?)=(.*)/);
-        cookies.push({
-            name: arg_cookie[1].trim(),
-            value: arg_cookie[2].trim()
-        });
-    });
-    rl.question("Id du livre: ", (arg_id) => {
-        rl.question("Télécharger en quel format ? [epub/pdf]: ", (arg_type) => {
-            try {
-                downloadBook(arg_id, arg_type);
-            }
-            catch(ex) {
-                console.log('Une erreur est survenue.');
-            }
-        });
-    });
-});
-
-async function downloadBook(bookID, type="pdf") {
+(async() => {
+    
     const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('http://www.eni-training.com/');
-    await page.setCookie(...cookies);
-    await page.goto('http://www.eni-training.com/client_net/mediabook.aspx?idR=' + bookID);
+    const cookies = [];
 
-    const [name, isbn, ids] = await page.evaluate(() => {
-        ids = [];
-        document.querySelectorAll('ul#Root li:not(.quiz)').forEach((elem) => {
-            if(elem.id.match(/S_[0-9]+/)) ids.push(+elem.id.replace('S_','')); 
-        });
-        
-        let isbn = null;
-        if(document.querySelector('.Infos') && document.querySelector('.Infos').innerText.match('ISBN : (.*)')) {
-            isbn = document.querySelector('.Infos').innerText.match('ISBN : (.*)')[1];
-        }
-
-        let name = null;
-        if(document.querySelector("#Menu strong")) {
-            name = document.querySelector("#Menu strong").innerText;
-        }
-
-        if(ids.length < 3 || !name) {
-            throw new Error('Une erreur est survenue');
-        }
-
-        return Promise.resolve([name, isbn, ids]);
-    });
-
-    fsExtra.emptyDirSync('tmp');
-
-    for (const [index, id] of ids.entries()){
-        await page.evaluate((id) => {
-            window._r = id;
-            eni.sc.ck.s('__hnwky', $.now(), 1);
-            eni.sc.ck.s('__rsaxc', window._r, 1);
-            document.querySelector('#S_' + window._r).click();
-            return Promise.resolve();
-        }, id);
-
-        // Le temps que la page charge
-        await new Promise(function(resolve) {setTimeout(resolve, 1500)});
-
-        const pageDL = await browser.newPage();
-        await pageDL.goto('http://www.eni-training.com/client_net/pdfexport.aspx?exporttype=1');
-        
-        if(type === "epub") {
-            await dlEPUB(pageDL, id);
-        } else {
-            await dlPDF(pageDL, id);
-        }
-
-        // Le temps de DL et de passer à la page suivante
-        await new Promise(function(resolve) {setTimeout(resolve, 500)});
-        console.log((index + 1) + "/" + ids.length);
-    }
-
-    if(type === "epub") {
-        await createEPUB(name, isbn);
-    } else {
-        await mergePDF(name);
-    }
-    fsExtra.emptyDirSync('tmp');
-    await browser.close();
-    console.log("\n Votre livre a été téléchargé, il se trouve ici : result/" + name + "." + type);
-}
-
-async function dlEPUB(page, id) {
-    let content = await page.content();
-    content = content.replace(/src=(|\"|\')\.\.\//g, 'src=$1http://www.eni-training.com/');
-    
-    content = content.replace('width:99%;', 'width:calc(100% - 24px);');
-    content = content.replace("<body><div", "<body><div style='padding:25px'");
-
-    fs.writeFileSync('tmp/' + id + '.html', content);
-    page.close();
-}
-
-async function createEPUB(name, isbn) {
-
-    const options = {
-        title: name,
-        output: 'result/' + name + '.epub',
-        css: '',
-        appendChapterTitles: false,
-        tocTitle: 'Sommaire',
-        content: []
-    };
-
-    const cover = await getCover(isbn);
-    if(cover) {
-        options.cover = cover;
-    }
-
-    let first = 0;
-    fs.readdirSync('tmp').forEach(file => {
-        const html = parser.parse(fs.readFileSync('tmp/' + file,'utf8'));
-
-        if(!first) {
-            html.querySelectorAll('style').forEach((style) => {
-                options.css += style.innerHTML;
+    rl.question("Cookies: ", (arg_cookies) => {
+        arg_cookies = arg_cookies.replace(/"/g, "").replace(/\s/g, "").split(";")
+        arg_cookies.forEach((arg_cookie) => {
+            arg_cookie = arg_cookie.match(/(.*?)=(.*)/);
+            cookies.push({
+                name: arg_cookie[1].trim(),
+                value: arg_cookie[2].trim()
             });
-            first = 1;
+        });
+        showBooks();
+    });
+
+    async function showBooks() {
+        const page = await browser.newPage();
+        await page.goto('http://www.eni-training.com/');
+        await page.setCookie(...cookies);
+        await page.goto('http://www.eni-training.com/client_net/cshome.aspx?p=1&lyt=2&type=2&order=5&nbRbP=24&typeLst=CS&ct=cs');
+
+        await new Promise(function(resolve) {setTimeout(resolve, 1500)});
+        const livres = await page.evaluate(() => {
+            livres = [];
+            document.querySelectorAll('#Resources a').forEach((livre) => {
+                livres.push({
+                    id: +livre.dataset.id,
+                    nom: livre.querySelector('h2').innerText.replace('\n', ' - ')
+                });
+            });
+            return Promise.resolve(livres);
+        });
+        
+        console.log("\n");
+
+        livres.forEach((livre, index) => {
+            console.log(`[${index + 1}] ${livre['nom']}`);
+        });
+
+        rl.question("Quel livre télécharger ? ", (arg_id_dl) => {
+            rl.question("Télécharger en quel format ? [epub/pdf]: ", (arg_type) => {
+                try {
+                    downloadBook(livres[(+arg_id_dl - 1)].id, arg_type)
+                }
+                catch(ex) {
+                    console.log('Une erreur est survenue.');
+                }
+            });
+        });
+    }
+
+    async function downloadBook(bookID, type="pdf") {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('http://www.eni-training.com/');
+        await page.setCookie(...cookies);
+        await page.goto('http://www.eni-training.com/client_net/mediabook.aspx?idR=' + bookID);
+
+        await new Promise(function(resolve) {setTimeout(resolve, 1500)});
+        const [name, isbn, ids] = await page.evaluate(() => {
+            ids = [];
+            document.querySelectorAll('ul#Root li:not(.quiz)').forEach((elem) => {
+                if(elem.id.match(/S_[0-9]+/)) ids.push(+elem.id.replace('S_','')); 
+            });
+            
+            let isbn = null;
+            if(document.querySelector('.Infos') && document.querySelector('.Infos').innerText.match('ISBN : (.*)')) {
+                isbn = document.querySelector('.Infos').innerText.match('ISBN : (.*)')[1];
+            }
+
+            let name = null;
+            if(document.querySelector("#Menu strong")) {
+                name = document.querySelector("#Menu strong").innerText;
+            }
+
+            if(ids.length < 3 || !name) {
+                console.log('Une erreur est survenue. Vous devriez surement vous reconnecter.');
+                process.exit(0);
+            }
+
+            return Promise.resolve([name, isbn, ids]);
+        });
+
+        fsExtra.emptyDirSync('tmp');
+
+        for (const [index, id] of ids.entries()){
+            await page.evaluate((id) => {
+                window._r = id;
+                eni.sc.ck.s('__hnwky', $.now(), 1);
+                eni.sc.ck.s('__rsaxc', window._r, 1);
+                document.querySelector('#S_' + window._r).click();
+                return Promise.resolve();
+            }, id);
+
+            // Le temps que la page charge
+            await new Promise(function(resolve) {setTimeout(resolve, 1500)});
+
+            const pageDL = await browser.newPage();
+            await pageDL.goto('http://www.eni-training.com/client_net/pdfexport.aspx?exporttype=1');
+            
+            if(type === "epub") {
+                await dlEPUB(pageDL, id);
+            } else {
+                await dlPDF(pageDL, id);
+            }
+
+            // Le temps de DL et de passer à la page suivante
+            await new Promise(function(resolve) {setTimeout(resolve, 500)});
+            console.log((index + 1) + "/" + ids.length);
         }
-        
-        html.querySelectorAll('var').forEach((element) => {
-            element.remove();
-        });
 
-        const title = html.querySelector('h1.title').innerText;
-        //html.querySelector('h1.title').remove();
-
-        options.content.push({
-            title,
-            data: html.querySelector('body').innerHTML,
-        });
-        
-    });
-    if (!fs.existsSync('result')){
-        fs.mkdirSync('result');
+        if(type === "epub") {
+            await createEPUB(name, isbn);
+        } else {
+            await mergePDF(name);
+        }
+        fsExtra.emptyDirSync('tmp');
+        await browser.close();
+        console.log("\nVotre livre a été téléchargé, il se trouve ici : result/" + name + "." + type);
+        process.exit(0);
     }
-    new epub(options).promise.then(() => {
-        return Promise.resolve();
-    });
-}
 
-async function dlPDF(page, id) {
-    await page.evaluate(() => {
-        document.querySelectorAll('.MEDIABook_CallBack_PrgContent_Container .programlisting').forEach((elem) => {
-            elem.style.width = "calc(100% - 24px)";
-        });
-        return Promise.resolve();
-    });
-    
-    await page.pdf({path: 'tmp/' + id + '.pdf', format: 'A4', margin: { left: '2cm', top: '2cm', right: '2cm', bottom: '2cm' }});
-    page.close();
-}
+    async function dlEPUB(page, id) {
+        let content = await page.content();
+        content = content.replace(/src=(|\"|\')\.\.\//g, 'src=$1http://www.eni-training.com/');
+        
+        content = content.replace('width:99%;', 'width:calc(100% - 24px);');
+        content = content.replace("<body><div", "<body><div style='padding:25px'");
 
-async function mergePDF(name) {
-    const merger = new PDFMerger();
-    fs.readdirSync('tmp').forEach(file => {
-        merger.add('tmp/' + file);
-    });
-    if (!fs.existsSync('result')){
-        fs.mkdirSync('result');
+        fs.writeFileSync('tmp/' + id + '.html', content);
+        page.close();
     }
-    await merger.save('result/' + name + '.pdf');
-}
 
-async function getCover(isbn) {
+    async function createEPUB(name, isbn) {
+        const options = {
+            title: name,
+            output: 'result/' + name + '.epub',
+            css: '',
+            appendChapterTitles: false,
+            tocTitle: 'Table des matières',
+            content: []
+        };
 
-    const options = {
-        url: 'https://www.editions-eni.fr/ajax/search.prvw.aspx',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'Accept': 'text/html, */*; q=0.01',
-            'X-Requested-With': 'XMLHttpRequest',
-            'Origin': 'https://www.editions-eni.fr'
-        },
-        body: 'exp=' + isbn
-    };
-    
-    return new Promise((resolve) => {
-        request(options, (error, response, body) => {
-            const html = parser.parse(body);
+        const cover = await getCover(isbn);
+        if(cover) {
+            options.cover = cover;
+        }
+
+        const infos = await getInfos(isbn);
+        if(infos) {
+            options.author = infos.author;
+            options.publisher = infos.publisher;
+            options.lang = infos.lang;
+            options.description = infos.description;
+            options.date = infos.date;
+        } else {
+            options.publisher = 'Éditions ENI.';
+            options.lang = 'fr';
+        }
+
+        let first = 0;
+        fs.readdirSync('tmp').forEach(file => {
+            const html = parser.parse(fs.readFileSync('tmp/' + file,'utf8'));
+
+            if(!first) {
+                html.querySelectorAll('style').forEach((style) => {
+                    options.css += style.innerHTML;
+                });
+                first = 1;
+            }
+            
+            html.querySelectorAll('var').forEach((element) => {
+                element.remove();
+            });
+
+            const title = html.querySelector('h1.title').innerText;
+
+            options.content.push({
+                title,
+                data: html.querySelector('body').innerHTML,
+            });
+            
+        });
+        if (!fs.existsSync('result')){
+            fs.mkdirSync('result');
+        }
+        return new Promise((resolve) => {
+            new epub(options).promise.then(resolve);
+        });
+    }
+
+    async function dlPDF(page, id) {
+        await page.evaluate(() => {
+            document.querySelectorAll('.MEDIABook_CallBack_PrgContent_Container .programlisting').forEach((elem) => {
+                elem.style.width = "calc(100% - 24px)";
+            });
+            return Promise.resolve();
+        });
+        
+        await page.pdf({path: 'tmp/' + id + '.pdf', format: 'A4', margin: { left: '2cm', top: '2cm', right: '2cm', bottom: '2cm' }});
+        page.close();
+    }
+
+    async function mergePDF(name) {
+        const merger = new PDFMerger();
+        fs.readdirSync('tmp').forEach(file => {
+            merger.add('tmp/' + file);
+        });
+        if (!fs.existsSync('result')){
+            fs.mkdirSync('result');
+        }
+        await merger.save('result/' + name + '.pdf');
+    }
+
+    async function getCover(isbn) {
+        const options = {
+            url: 'https://www.editions-eni.fr/ajax/search.prvw.aspx',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'Accept': 'text/html, */*; q=0.01',
+                'X-Requested-With': 'XMLHttpRequest',
+                'Origin': 'https://www.editions-eni.fr'
+            },
+            body: 'exp=' + isbn
+        };
+        
+        return new Promise((resolve) => {
+            request(options, (error, response, body) => {
+                const html = parser.parse(body);
+                try {
+                    let url = html.querySelector('img').attributes['src'];
+                    url = url.replace(/^\/\//, 'https://');
+                    url = url.replace(/\_S\./, '_XL.');
+                    resolve(url);
+                } catch(ex) {
+                    resolve(null);
+                }
+            });
+        });
+        
+    }
+
+    async function getInfos(isbn) {
+        return new Promise((resolve) => {
             try {
-                let url = html.querySelector('img').attributes['src'];
-                url = url.replace(/^\/\//, 'https://');
-                url = url.replace(/\_S\./, '_XL.');
-                resolve(url);
+                request({
+                    url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:' + isbn,
+                    method: 'GET'
+                }, (error, response, body) => {
+                    body = JSON.parse(body);
+                    if(!body['items'] || body['items'].length === 0 || !body['items'][0]['selfLink']) {
+                        resolve(null);
+                        return;
+                    }
+                    request({
+                        url: body['items'][0]['selfLink'],
+                        method: 'GET'
+                    }, (error, response, actual_body) => {
+                        actual_body = JSON.parse(actual_body);
+                        if(!actual_body['volumeInfo']) {
+                            resolve(null);
+                            return;
+                        }
+                        resolve({
+                            title: actual_body['volumeInfo']['title'],
+                            subtitle: actual_body['volumeInfo']['subtitle'],
+                            author: actual_body['volumeInfo']['authors'],
+                            publisher: actual_body['volumeInfo']['publisher'],
+                            date: new Date(body['items'][0]['volumeInfo']['publishedDate']).toISOString(),
+                            description: actual_body['volumeInfo']['description'],
+                            lang: actual_body['volumeInfo']['language'],
+                        });
+                    });
+                });
             } catch(ex) {
                 resolve(null);
             }
         });
-    });
-    
-}
+    }
+})()
